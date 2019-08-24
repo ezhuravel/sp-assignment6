9cfd2a553bd7ef3e995755e9f0a5d8fec53bb9ee
ezhuravel <ezhuravel@gmail.com>
2019-08-24 13:57:04 -0400

    removed testing statements, added histogram

--- vispums.py
+++ vispums.py
@@ -7,6 +7,7 @@
 import sys
 import os
 import pandas as pd 
+import numpy as np
 import matplotlib.pyplot as plt
 
 # Entry point of application
@@ -27,32 +28,32 @@ def main(argv):
     
     # Get English only 
     english_only = ((len(gp.get_group(1))/totalSpokenlanguages) * 100)
-    print(english_only)
 
     # Get Spanish
     spanish = ((len(gp.get_group(2))/totalSpokenlanguages) * 100)
-    print(spanish)
     
     # Get Other Indo-European languages
     european = ((len(gp.get_group(3))/totalSpokenlanguages) * 100)
-    print(european)
   
     # Get Asian
     asian = ((len(gp.get_group(4))/totalSpokenlanguages) * 100)
-    print(asian)
     
     # Get Other
     other = ((len(gp.get_group(5))/totalSpokenlanguages) * 100)
-    print(other)
     
     labels = "English only", "Spanish", "Other Indo-European languages", "Asian and Pacific Island languages", "other"
     sizes = [english_only, spanish, european, asian,other]
     
-    fig1, ax1 = plt.subplots()
+    
+    fig1, (ax1, ax2) = plt.subplots(ncols=2)
     ax1.pie(sizes, shadow=False, startangle=242)
-    ax1.axis('equal') 
-    ax1.legend(labels, loc='upper left')
-
+    ax1.set_ylabel('HHL')
+    ax1.legend(labels, loc="upper left")
+    ax1.title.set_text("Household Languages")
+        
+    ax2.hist(pums_data["HINCP"].dropna(),bins=np.logspace(np.log10(1),np.log10(10000000), 50), facecolor='green', alpha=0.5)
+    ax2.title.set_text("Distribution of Houshold Income")
+    ax2.set_xscale("log")
     plt.show()
 
     
