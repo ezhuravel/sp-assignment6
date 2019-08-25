af3cb94363a056143dff71f2caa919e7c6c7f06d
ezhuravel <ezhuravel@gmail.com>
2019-08-25 11:34:00 -0400

    added rest of plots to main

--- vispums.py
+++ vispums.py
@@ -10,6 +10,19 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Create mappings for easy TAXP transformation
+mappings = np.append(np.array([0, np.nan, 1]), np.arange(50, 1050, 50))
+mappings = np.append(mappings, np.arange(1100, 5100, 100))
+mappings = np.append(mappings, np.array([5500, 6000, 7000, 8000, 9000, 10000]))
+
+
+# Helper function for changing values
+def map_taxp(x):
+    try:
+        return mappings[int(x)]
+    except ValueError:
+        return x
+
 # Entry point of application
 def main(argv):
     print("CPSC-51100, Summer 2019")
@@ -45,17 +58,69 @@ def main(argv):
     sizes = [english_only, spanish, european, asian,other]
     
     
-    fig1, (ax1, ax2) = plt.subplots(ncols=2)
+    # figure for containing all the subplots
+    figure = plt.figure(figsize=(12, 8))
+    # Add the ie chart subplot
+    
+    ax1 = figure.add_subplot(2, 2, 1)
+    ax1.pie(sizes, shadow=False, startangle=242)
+    ax1.axis('equal')
     ax1.pie(sizes, shadow=False, startangle=242)
     ax1.set_ylabel('HHL')
     ax1.legend(labels, loc="upper left")
     ax1.title.set_text("Household Languages")
         
+    ax2 = figure.add_subplot(2, 2, 2)
     ax2.hist(pums_data["HINCP"].dropna(),bins=np.logspace(np.log10(1),np.log10(10000000), 50), facecolor='green', alpha=0.5)
     ax2.title.set_text("Distribution of Houshold Income")
     ax2.set_xscale("log")
-    plt.show()
+   
 
     
+     # Plot the household vehicle data
+    # Get the household vehicles data
+    vehicles = pd.DataFrame(pums_data[['VEH', 'WGTP']])
+    # Remove NaN values
+    vehicles.dropna(axis=0, inplace=True)
+    # Group the vehicles by number and sum to account for household size
+    groups = vehicles.groupby(['VEH']).sum() / 1000
+    # Create plotting information
+    x_points = np.array(groups['WGTP'].index.tolist())
+    y_values = np.array(groups['WGTP'])
+    # Add new Axes object to bottom left and set attributes
+    ax3 = figure.add_subplot(2, 2, 3)
+    ax3.bar(x_points, y_values, color='r')
+    ax3.set_title("Vehicles Available in Households")
+    ax3.set_xlabel("# of Vehicles")
+    ax3.set_ylabel("Thousands of Households")
+    ax3.yaxis.get_major_ticks()[0].set_visible(False)
+
+    # Plot the scatter plot
+    # Get the tax data
+    data = pd.DataFrame(pums_data[['TAXP', 'VALP', 'WGTP', 'MRGP']]).dropna()
+    # Map tax code to lower bound tax category
+    data['TAXP'] = data['TAXP'].apply(map_taxp)
+    # Get data for scatter plot and create
+    x_vals = np.array(data['TAXP'])
+    y_vals = np.array(data['VALP'])
+    widths = np.array(data['WGTP'])
+    colors = np.array(data['MRGP']).astype(float)
+    ax4 = figure.add_subplot(2, 2, 4)
+    sc = ax4.scatter(y_vals, x_vals, s=widths, c=colors, cmap=plt.cm.bwr, marker='o')
+    ax4.set_xlim([0, 1200000])
+    ax4.set_ylim([0, 10500])
+    ax4.margins(x=0)
+    ax4.set_title("Property Taxes vs. Property Values")
+    ax4.set_xlabel("Property Value ($)")
+    ax4.set_ylabel("Taxes ($)")
+    ax5 = plt.colorbar(sc)
+    ax5.set_label("First Mortgage Payment (Monthly $)")
+    ax5.set_ticks([1250, 2500, 3750, 5000])
+
+    # Set layout to tight, and save as png
+    plt.tight_layout()
+    #plt.savefig("pums.png", type='png')
+    plt.show()
+     
 if __name__ == "__main__":
     main(sys.argv)
